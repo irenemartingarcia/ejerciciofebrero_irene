8b4cd7465a61ebffd04bce3f34e58ba8741aebf3
Usuario <irene.martingarcia@edu.uah>
2025-02-12 19:59:42 +0100

    1ºprueba de los test

---
+++ Test/java/es/uah/matcomp/mp/e1/ejerciciosclases/CircleTest.java
@@ -0,0 +1,37 @@
+package es.uah.matcomp.mp.e1.ejerciciosclases;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class CircleTest {
+
+    @Test
+    void getRadius() {
+        Circle c=new Circle(1.1);
+        assertEquals(1.1, c.getRadius(),"El radio deberia ser 1.1");
+    }
+
+    @Test
+    void setRadius(){
+        Circle c= new Circle(1.1);
+        c.setRadius(6.9);
+        assertEquals(6.9, c.getRadius(),"El radio deberia deberia ser 6.9");
+    }
+
+    @Test
+    void getArea() {
+        Circle c= new Circle(1.0);
+        assertEquals(Math.PI, c.getArea(),"El area debería ser pi");
+    }
+
+    @Test
+    void getCircumference() {
+        Circle c= new Circle();
+        assertEquals(2*Math.PI, c.getCircumference(),"El perímetro debería ser 2pi");
+    }
+
+    @Test
+    void testToString() {
+        Circle c= new Circle(1.0);
+        assertEquals("Circle[radius="+1.0+"]",c.toString());
+    }
+}
---
+++ Test/java/es/uah/matcomp/mp/e1/ejerciciosclases/EmployeeTest.java
@@ -0,0 +1,65 @@
+package es.uah.matcomp.mp.e1.ejerciciosclases;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeTest {
+
+    @Test
+    void getId() {
+        Employee e= new Employee(12, "Samuel","García", 3455);
+        assertEquals(12,e.getId(),"El id debería ser 12");
+    }
+
+    @Test
+    void getSalary() {
+        Employee e= new Employee(12, "Samuel","García", 3455);
+        assertEquals(3455,e.getSalary(),"El salario debería ser 3455");
+
+    }
+
+    @Test
+    void getFirstName() {
+        Employee e= new Employee(12, "Samuel","García", 3455);
+        assertEquals("Samuel",e.getFirstName(),"el nombre debería ser Samuel");
+    }
+
+    @Test
+    void getLastName() {
+        Employee e= new Employee(12, "Samuel","García", 3455);
+        assertEquals("García",e.getLastName(),"el apellido debería ser García");
+    }
+
+    @Test
+    void setSalary() {
+        Employee e= new Employee(12, "Samuel","García", 3455);
+        e.setSalary(2000);
+        assertEquals(2000,e.getSalary(),"el salario debería ser 2000");
+    }
+
+    @Test
+    void getName() {
+        Employee e= new Employee(12, "Samuel","García", 3455);
+        assertEquals("Samuel García",e.getName(),"el nombre y apellido debería ser:Samuel García");
+    }
+
+    @Test
+    void getAnnualSalary() {
+        Employee e= new Employee(12, "Samuel","García", 2);
+        assertEquals(24,e.getAnnualSalary(),"el salario anual debería ser 24");
+    }
+
+    @Test
+    void raiseSalary() {
+        Employee e= new Employee(12, "Samuel","García", 100);
+        e.raiseSalary(50);
+        assertEquals(150,e.getSalary(),"El salario debería ser 50");
+    }
+
+    @Test
+    void testToString() {
+        Employee e= new Employee(12, "Samuel","García", 3455);
+        assertEquals("Employee[id=12,name=Samuel García,salary=3455]",e.toString(),"El mensaje debería ser:Employee[id=12,name=Samuel García,salary=3455]");
+    }
+}
