ab7bbf0e59bce129c3054db92147a560514357d5
Usuario <irene.martingarcia@edu.uah>
2025-02-12 16:52:11 +0100

    continuacion10

--- src/es/uah/matcomp/mp/e1/ejerciciosclases/Ball.java
+++ src/es/uah/matcomp/mp/e1/ejerciciosclases/Ball.java
@@ -1,25 +1,76 @@
 package es.uah.matcomp.mp.e1.ejerciciosclases;
 
-public class Ball {
+public class Ball{
     private float x;
     private float y;
     private int radius;
     private float xDelta;
     private float yDelta;
-    public Ball(float x,float y, int radius,float xDelta, float yDelta){
+
+    public Ball(float x, float y,int radius, float xDelta, float yDelta) {
         this.x=x;
         this.y=y;
         this.radius=radius;
         this.xDelta=xDelta;
         this.yDelta=yDelta;
     }
+
     public float getX() {
         return x;
     }
-    public void setX(float x) {
-        this.x = x;
-    }
+
     public float getY() {
         return y;
     }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public float getXDelta() {
+        return xDelta;
+    }
+
+    public float getYDelta() {
+        return yDelta;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public void setXDelta(float xDelta) {
+        this.xDelta = xDelta;
+    }
+
+    public void setYDelta(float yDelta) {
+        this.yDelta = yDelta;
+    }
+
+    public void move(){
+        this.x += xDelta;
+        this.y += yDelta;
+    }
+
+    public void reflectHorizontal(){
+        this.xDelta = -xDelta;
+    }
+
+    public void reflectVertical(){
+        this.yDelta = -yDelta;
+    }
+
+    @Override
+    public String toString() {
+        return "Ball[(" + x + "," + y + "), speed=(" + xDelta + "," + yDelta + ")]";
+    }
 }
+
